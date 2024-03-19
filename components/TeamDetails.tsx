f51441657b660513130c04e0c8f3411ddc2381de
ProtoCyte <billyjia1@hotmail.com>
2024-03-19 14:09:23 -0700

    temp completed

--- components/TeamDetails.tsx
+++ components/TeamDetails.tsx
@@ -16,7 +16,7 @@ const TeamDetails = ({isOpen, closeModal, teamId}: TeamStatsProps) => {
   useEffect(() => {
     const fetchTeamStats = async () => {
       const headers = {
-        'X-RapidAPI-Key': 'e90e8d5fadmsha0ee8f6cfe54816p11b926jsn5a2df73b01f7',
+        'X-RapidAPI-Key': '',
         'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
       }
         const response = await fetch(`https://api-nba-v1.p.rapidapi.com/teams/statistics?id=${teamId}&season=2023`, {headers: headers,});
